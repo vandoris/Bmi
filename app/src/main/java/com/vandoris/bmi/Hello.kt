1d39c75795650e93d419856c8a65deb7abcebce4
vandoris <vandoris@gmail.com>
2024-08-12 12:51:07 +0800

    constructor

--- app/src/main/java/com/vandoris/bmi/Hello.kt
+++ app/src/main/java/com/vandoris/bmi/Hello.kt
@@ -6,8 +6,14 @@ class Hello {
 }
 
 fun main() {
-    val p = Person()
-    p.hello()
+    val p = Person("vandoris",65.5f, 1.7f)
+    println(p.name + " bmi is : " + p.bmi())
+
+    //BMI
+    val weight = 65.5f
+    val height = 1.7f
+    val bmi = weight / (height * height)
+    println("BMI : " + bmi)
 
     println("Hello Kotlin!")
 
--- app/src/main/java/com/vandoris/bmi/Person.kt
+++ app/src/main/java/com/vandoris/bmi/Person.kt
@@ -1,6 +1,11 @@
 package com.vandoris.bmi
 
-class Person {
+class Person (val name:String, val weight:Float, val height:Float){
+
+    fun bmi() : Float {
+        val bmi = weight / (height * height)
+        return bmi
+    }
     fun hello() {
         println("Hello")
     }
